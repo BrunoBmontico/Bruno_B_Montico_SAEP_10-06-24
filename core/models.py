745a8696e981f286246a121abd7563d324c5e78d
BrunoBmontico <aluno103.23187204bruno.com>
2024-06-10 19:05:50 -0300

    models criado

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Turma(models.Model):
+    nome_turma = models.CharField(verbose_name='Turma', max_length=99, null=True, blank=True)
+    nome_professor = models.CharField(verbose_name='Nome Professor', max_length=99, null=True, blank=True)
+    id_professor = models.ForeignKey(User, verbose_name='Id-Professor', on_delete=models.CASCADE, null=True, blank=True)
+
+class Atividades(models.Model):
+    nome_atividade = models.CharField(verbose_name='Atividade', max_length=99, null=True, blank=True)
+    id_professor = models.ForeignKey(User, verbose_name='Id-Professor', on_delete=models.CASCADE, null=True, blank=True )
+    nome_professor = models.CharField(verbose_name='Nome Professor', max_length=99, null=True, blank=True)
+    id_turma = models.ForeignKey(Turma, verbose_name='Id-Turma', on_delete=models.CASCADE, null=True, blank=True)
+    nome_turma = models.CharField(verbose_name='Nome Professor', max_length=99, null=True, blank=True)
