755f7b040c4707f1e200a0f748806a158c61d9c3
BrunoBmontico <aluno103.23187204bruno.com>
2024-06-10 19:50:06 -0300

    templates e views criadas

--- core/views.py
+++ core/views.py
@@ -1,3 +1,136 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect                    # REQUESTS PADROES DJANGO
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout                         # REQUERIMENTO DE LOGIN
+from core.models import *
+
 
 # Create your views here.
+def login_professor(request):
+    if request.method == 'GET':
+        return render(request, 'core/login.html')
+    else:
+        email_professor = request.POST.get('email_professor')
+        senha_professor = request.POST.get('senha_professor')
+
+        usuario = authenticate(username=email_professor, password=senha_professor)
+
+        if usuario:
+            login(request, usuario)
+            return redirect(f'/tela_professor/{request.user.id}')
+        
+        else:
+            return HttpResponse('E-mail ou Senha incorretos')
+        
+# logout
+def logout_usuario(request):
+    logout(request)
+    return redirect('/')
+        
+#Cadastrar Professor
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'core/cadastro.html')
+    else:
+        nome_professor = request.POST.get('nome_professor')
+        sobrenome_professor = request.POST.get('sobrenome_professor')
+        senha_professor = request.POST.get('senha_professor')
+        email_professor = request.POST.get('email_professor')
+
+        if nome_professor and sobrenome_professor and senha_professor and email_professor !='':
+            usuario = User.objects.filter(username=email_professor).first()
+            if usuario:
+                return HttpResponse('Esse E-mail ja existe!')
+            else:
+                usuario = User.objects.create_user(
+
+                    first_name=nome_professor,
+                    last_name=sobrenome_professor,
+                    email=email_professor,
+                    username=email_professor, 
+                )
+
+                usuario.set_password(senha_professor)
+                usuario.save()
+                return redirect('/')
+            
+#Cadastro Turma
+def cadastro_turma(request, id):
+    usuario = get_object_or_404(User, pk=id)
+    if request.method == 'GET':
+        return render(request, 'core/cadastro_turma.html')
+    else:
+        nome_Turma = request.POST.get('nome_turma')
+
+        if nome_Turma != '':
+            turma = Turma.objects.create(
+                nome_turma = nome_Turma,
+                id_professor = usuario,
+                First_Name = usuario.first_name,
+            )
+            turma.save()
+            return redirect(f'/tela_professor/{request.user.id}')
+        else:
+            return HttpResponse('Verifique os campos')
+
+#area da turma
+def area_turma(request, id):
+    if request.method == 'GET':
+        turma = get_object_or_404(Turma, pk=id)
+        atividades = Atividade.objects.all()
+
+        atividades_turma = atividades.filter(id_turma=turma)
+
+        context = {
+            'turma': turma,
+            'atividades_turma': atividades_turma,
+        }
+        return render(request, 'core/tela_turma.html', context)
+
+#cadastro Atividades
+def cadastro_atividades(request, id_professor, id_turma):
+    usuario = get_object_or_404(User, pk=id_turma)
+    turma = get_object_or_404(Turma, pk=id_turma)
+
+    context = {
+        'usuario': usuario,
+        'turma': turma
+    }
+
+    if request.method == 'GET':
+        return render(request, 'core/cadastro_atividades.html', context)
+    else:
+        nome_atividade = request.POST.get('Nome_Atividade')
+
+        atividade = Atividade.objects.create(
+            nome_atividade = nome_atividade,
+            id_professor = usuario,
+            nome_professor = usuario.first_name,
+            id_turma = turma,
+            nome_turma = turma.Nome_Turma,
+        )
+        atividade.save()
+        return redirect(f'/tela_turma/{turma.id}')
+
+#area do professor
+def area_professor(request, id):
+    if request.method == 'GET':
+        turmas = Turma.objects.all()
+        turmas_professor = turmas.filter(id_professor=request.user.id)
+        return render(request, 'core/tela_professor.html', {'turmas_professor': turmas_professor})
+    
+
+#excluir turma
+def excluir_turma(request, id_turma):
+    turma = get_object_or_404(Turma, pk=id_turma)
+    atividades = Atividade.objects.all()
+
+    for atividade in atividades:
+        if atividade.id_lista != turma:
+            turma.delete()
+            return redirect(f'/tela_professor/{request.user.id}')
+        else:
+            return HttpResponse('Essa turma tem atividades não e possivel excluir')
+
+def confirmar(request, id_turma):
+    turma = get_object_or_404(Turma, pk=id_turma)
+    return render(request, 'core/confirmar_excluir.html',{'turma':turma})
--- setup/urls.py
+++ setup/urls.py
@@ -1,23 +1,16 @@
-"""
-URL configuration for setup project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', views.login_professor, name='login_professor'),
+    path('logout/', views.logout_usuario, name='logout'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('cadastrar_turma/<int:id>/', views.cadastro_turma, name='cadastrar_turma'),
+    path('cadastrar_atividade/<int:id_usuario>/<int:id_turma>/', views.cadastro_atividades, name='cadastrar_atividade'),
+    path('tela_professor/<int:id>/', views.area_professor, name='area_professor'),
+    path('tela_turma/<int:id>', views.area_turma, name='area_turma'),
+    path('deletar/<int:id_turma>/', views.excluir_turma, name='deletar' ),
+    path('confirmar/<int:id_turma>/', views.confirmar, name='confirmar')
 ]
